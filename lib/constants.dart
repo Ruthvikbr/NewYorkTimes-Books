1261ea31386ec4f0b881947636cc02fb86fb1f15
Ruthvikbr <ruthvikbr24@gmail.com>
2022-01-23 16:30:20 +0530

    Fix workflow

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const String apiKey = "INSERT YOUR API KEY HERE";
+  static const String bestSellersUrl =
+      "https://api.nytimes.com/svc/books/v3/lists.json?list=Hardcover%20Fiction";
+}
